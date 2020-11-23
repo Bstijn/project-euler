db995a1aa8f8f7dcdddd06dc471c9aa265bcaebf
Bstijn <baltessen.stijn@gmail.com>
2020-11-23 00:07:35 +0000

    init and euler 3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Int64 number = 600851475143;
+        List<int> primefactor = new List<int>();
+        while (number > 1){
+            Console.WriteLine(number);
+            bool nextprimefound = false;
+            int i = 2; 
+            while(nextprimefound == false)
+            {
+               if(number % i == 0)
+                {
+                    nextprimefound = true;
+                    number /= i;
+                    primefactor.Add(i);
+                }
+                else
+                {
+                    i++;
+                    //ignore
+                }
+            }
+        }
+        foreach(var primes in primefactor)
+        {
+            Console.WriteLine(primes);
+        }
+    }
+}
