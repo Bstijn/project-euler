9a63ead8bee771c70ded84f80ae87ccb78b1e70e
Bstijn <baltessen.stijn@gmail.com>
2020-11-23 21:34:11 +0000

    euler 12 and 13

--- ConsoleApp1/Euler 12.cs
+++ ConsoleApp1/Euler 12.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace ConsoleApp1
@@ -8,7 +9,46 @@ namespace ConsoleApp1
     {
         public void Run()
         {
+            Int64 number = 1;
+            Int64 nextnumber = 2;
+            bool found = false;
+            int highestdivisiros = 0;
+            while (!found)
+            {
+                number += nextnumber;
+                nextnumber += 1;
+                var amountOfDivisors = CalculateAmountOfDivisors(number);
+                if(amountOfDivisors > highestdivisiros)
+                {
+                    highestdivisiros = amountOfDivisors;
+                    Console.WriteLine(amountOfDivisors);
+                }
+                if(amountOfDivisors >= 500)
+                {
+                    found = true;
+                }
+            }
+            Console.WriteLine(number);
+        }
 
+        public int CalculateAmountOfDivisors(Int64 number)
+        {
+            int nod = 0;
+            int sqrt = (int)Math.Sqrt(number);
+
+            for (int i = 1; i <= sqrt; i++)
+            {
+                if (number % i == 0)
+                {
+                    nod += 2;
+                }
+            }
+            if (sqrt * sqrt == number)
+            {
+                nod--;
+            }
+
+            return nod;
         }
     }
 }
